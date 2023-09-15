af348b391428cc3a1138d4830cb1316570a5ad76
Star <109367204+star-is-fruit@users.noreply.github.com>
2023-09-15 16:02:44 +0530

    Add files via upload

--- CS lab - week 2- program 11.cpp
+++ CS lab - week 2- program 11.cpp
@@ -12,6 +12,70 @@ int main(){
 	printf("Enter year: ");
 	scanf("%d", &y1);
 	
+	 
+	if(m1 == 1 || m1 == 3 || m1 == 5 || m1 == 7 || m1 == 8 || m1 == 10 || m1 == 12){
+		if(d1 < 31){
+			d2 = ++d1;
+			m2 = m1;
+			y2 = y1;
+		}
+		
+		else if(d1 == 31)
+			if(m1 != 12){
+				d2 = 1;
+				m2 = ++m1;
+				y2 = y1;
+			}
+			else{
+				d2 = 1;
+				m2 = 1;
+				y2 = ++y1;
+			}
+			
+	}
+	else if(m1 == 4 || m1 == 6 || m1 == 9 || m1 == 11){
+		if(d1 < 30){
+			d2 = ++d1;
+			m2 = m1;
+			y2 = y1;
+		}
+		else{
+			d2 = 1;
+			m2 = ++m1;
+			y2 = y1;
+		}
+		
+	}
+	else if(m1 == 2){
+		if(y1 % 4 == 0 && (y1 % 100 != 0 || y1 % 400 == 0)){
+			if(d1 < 29){
+			d2 = ++d1;
+			m2 = m1;
+			y2 = y1;
+			}
+			else{
+			d2 = 1;
+			m2 = ++m1;
+			y2 = y1;
+			}
+		}
+		else{
+			if(d1 < 28){
+			d2 = ++d1;
+			m2 = m1;
+			y2 = y1;
+			}
+			else{
+			d2 = 1;
+			m2 = ++m1;
+			y2 = y1;
+			}
+		}
+	}
+	else{
+		printf("Enter valid year and month");
+	}
 	
+	printf("The updated date: %d %d %d", d2, m2, y2);
 	return 0;
 }
