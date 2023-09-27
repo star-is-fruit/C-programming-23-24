19551f67cd6d09b2544b43ccebfd46de5b33bc9f
Star <109367204+star-is-fruit@users.noreply.github.com>
2023-09-27 20:31:22 +0530

    patch 1 w3-p15

--- week-3/program 15-f las dig.c
+++ week-3/program 15-f las dig.c
@@ -2,18 +2,21 @@
 #include<math.h>
 
 int main(){
-	int n, len;
+	int n, len, l;
 	
 	printf("Enter a number: ");
 	scanf("%d", &n);
+
+	l = n % 10;
 	
-	printf("Last digit of number is: %d\n", (n % 10));	
+	printf("Last digit of number is: %d\n", l);	
 	
 	while(n >= 10){
 		n /= 10;
 	}
 	
 	printf("First digit of number is: %d", n);
+	printf("Sum of first and last digits: %d", n + l);
 	
 	return 0;
 }
