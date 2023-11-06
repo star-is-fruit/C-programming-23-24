0882c0ca6675412290da4a9f19cf829b44cdeabf
Star <109367204+star-is-fruit@users.noreply.github.com>
2023-11-06 17:58:12 +0530

    Added character based programs

---
+++ week-3/program 3-uppercase.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	
+	for(int i = 0; i < 26; i++){
+		printf("%c\n", i+65);
+	}
+	
+	return 0;
+}
---
+++ week-3/program 4-lowercase.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	
+	for(int i = 0; i < 26; i++){
+		printf("%c\n", i+97);
+	}
+	
+	return 0;
+}
